40031da24f23668a7dae0858a0ebdbe9a93f7f81
david doria <dor15004@byui.edu>
2022-05-02 11:54:19 -0600

    Added prototype of property page

--- lib/Routes/properties.dart
+++ lib/Routes/properties.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'package:villadex/Util/nav_bar.dart';
 import 'package:villadex/Style/colors.dart';
@@ -15,13 +16,53 @@ class _PropertiesPageState extends State<PropertiesPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       /// Body
-      body: Container (
+      body: Container(
         color: VillaDexColors().background,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+
+            /// Spacing
+            const SizedBox(height: 45,),
+
+            /// Welcome Sign
+            Expanded(
+              flex: 2,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                child: Text(
+                  'Welcome, Diana',
+                  textAlign: TextAlign.center,
+                  textScaleFactor: 2.0,
+                  style: GoogleFonts.montserrat(
+                    textStyle: TextStyle(
+                      color: VillaDexColors().accent,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w300,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            /// Properties
+            Expanded(
+              flex: 11,
+              child: Container(
+                width: MediaQuery.of(context).size.width * .93,
+                color: VillaDexColors().accent,
+              ),
+            ),
+
+            /// Spacing
+            const SizedBox(height: 10,)
+          ],
+        ),
       ),
 
       /// Bottom Nav Bar
       bottomNavigationBar: const NavBar(),
     );
   }
-
 }
