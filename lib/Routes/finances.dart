4db6676e6f16adeea0272b4c29d0aad108d4945a
david doria <dor15004@byui.edu>
2022-05-03 16:49:06 -0600

    Added Pie Chart

--- lib/Routes/finances.dart
+++ lib/Routes/finances.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 import 'package:villadex/Util/nav_bar.dart';
 import 'package:villadex/Style/colors.dart';
+import 'package:villadex/Util/pie_chart.dart' as chart;
 
 class FinancesPage extends StatefulWidget {
   const FinancesPage({Key? key}) : super(key: key);
@@ -11,12 +13,46 @@ class FinancesPage extends StatefulWidget {
 }
 
 class _FinancesPageState extends State<FinancesPage> {
+  int touchedIndex = -1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       /// Body
-      body: Container (
+      body: Container(
         color: VillaDexColors().background,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            /// Spacing
+            const SizedBox(
+              height: 10,
+            ),
+
+            /// Pie Chart
+            const Expanded(
+              flex: 8,
+              child: chart.VilladexPieChart(title: 'Expenses',
+              ),
+            ),
+
+            /// Other
+            Expanded(
+              flex: 14,
+              child: Container(
+                width: MediaQuery.of(context).size.width * .93,
+                color: Colors.blue,
+              ),
+            ),
+
+            /// Spacing
+
+            const SizedBox(
+              height: 10,
+            )
+          ],
+        ),
       ),
 
       /// Bottom Nav Bar
@@ -24,4 +60,60 @@ class _FinancesPageState extends State<FinancesPage> {
     );
   }
 
+  /// Generates a list of sections to put in the pie chart
+  List<PieChartSectionData> showingSections() {
+    return List.generate(4, (i) {
+      final isTouched = i == touchedIndex;
+      final fontSize = isTouched ? 25.0 : 16.0;
+      final radius = isTouched ? 60.0 : 50.0;
+      switch (i) {
+        case 0:
+          return PieChartSectionData(
+            color: VillaDexColors().pieChartOne,
+            value: 40,
+            title: '40%',
+            radius: radius,
+            titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: VillaDexColors().pieChartText),
+          );
+        case 1:
+          return PieChartSectionData(
+            color: VillaDexColors().pieChartTwo,
+            value: 30,
+            title: '30%',
+            radius: radius,
+            titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: VillaDexColors().pieChartText),
+          );
+        case 2:
+          return PieChartSectionData(
+            color: VillaDexColors().pieChartThree,
+            value: 15,
+            title: '15%',
+            radius: radius,
+            titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: VillaDexColors().pieChartText),
+          );
+        case 3:
+          return PieChartSectionData(
+            color: VillaDexColors().pieChartFour,
+            value: 15,
+            title: '15%',
+            radius: radius,
+            titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: VillaDexColors().pieChartText),
+          );
+        default:
+          throw Error();
+      }
+    });
+  }
 }
--- lib/Style/colors.dart
+++ lib/Style/colors.dart
@@ -9,4 +9,11 @@ class VillaDexColors {
   Color primary    = const Color(0xFF698F3F);
   Color background = const Color(0xFFE8EEEE);
   Color accent     = const Color(0xFF695958);
+
+  /// Pie Chart Colors
+  Color pieChartText  = const Color(0xffffffff);
+  Color pieChartOne   = const Color(0xff0293ee);
+  Color pieChartTwo   = const Color(0xffd49444);
+  Color pieChartThree = const Color(0xff845bef);
+  Color pieChartFour  = const Color(0xff13d38e);
 }
